26a888f3d5e1afd5cc5ef5e124efdd2a309ca555
Akash-D7 <akashdurai473@gmail.com>
2024-11-11 22:31:41 +0530

    Add project files.

---
+++ Employee/Models/EmployeeModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Employee.Models
+{
+    public class EmployeeModel
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        [RegularExpression(@"^\+?\d{0}\-?\d{10}$", ErrorMessage = "Invalid Phone Number.")]
+        public string ContactNo { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Please select a date.")]
+        [Display(Name = "Selected Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime JoiningDate { get; set; }
+        public string projectName { get; set; }
+        public string RoleName { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
